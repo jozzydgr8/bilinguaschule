ab0d368d57b66fdd5be341e6a77681b512d8243b
jozzydgr8 <jozzydgreat1@gmail.com>
2026-08-01 03:05:57 -0700

    added hero

---
+++ src/pages/homePage/homeComponent/Hero.tsx
@@ -0,0 +1,40 @@
+import { FlatButton } from "../../../shared/FlatButton"
+import {CheckOutlined} from '@ant-design/icons';
+
+export const Hero = ()=>{
+    return(
+        <section id='hero'>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>
+                            Learn German <span style={{color:'var(--primary-blue)'}}>With Confidence</span>
+                        </h1>
+                        <br/>
+                        <p className='subtopic'>
+                            Flexible online German courses from A1 beginner to B1 intermediate. Our bilingual teachers explain concepts in English when needed, so you never feel lost. Master speaking, writing, and exam preparation at your pace.
+                        </p>
+                        <div>
+                            <FlatButton title='Start Learning Today' className=" btn btn-xl btnPrimary"/> <FlatButton title='see how it works' className='btn btn-xl btnSecondary'/>
+                        </div>
+                        <br/>
+
+                        <div className="row heroBottom">
+                            <div className="col-md-4 mb-2">
+                                <small style={{display:"flex", gap:'12px'}}><CheckOutlined/> Free trial lesson</small>
+                            </div>
+                            <div className="col-md-4 mb-2">
+                                <small style={{display:"flex", gap:'12px'}}><CheckOutlined/> Small group classes</small>
+
+                            </div>
+                            <div className="col-md-4 mb-2">
+                                <small style={{display:"flex", gap:'12px'}}><CheckOutlined/> Exam prep included</small>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
