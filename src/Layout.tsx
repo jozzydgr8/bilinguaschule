490c9f049e33f947b1debfbab1cbbfd3aaa38eca
jozzydgr8 <jozzydgreat1@gmail.com>
2026-08-07 20:08:50 -0700

    added chat us now button

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom"
 import { Navbar } from "./shared/Navbar"
 import { useEffect } from "react";
+import { handleRequest } from "./shared/handleRequest";
+import { FlatButton } from "./shared/FlatButton";
 
 export const Layout = ()=>{
      useEffect(()=>{
@@ -50,6 +52,25 @@ export const Layout = ()=>{
         <>
         <Navbar/>
         <Outlet/>
+       <FlatButton
+          className="whatsappIcon borderlessbtn"
+          onClick={() =>
+            handleRequest(
+              "Hi BilingualChule! 👋 I’m interested in learning German with you. I’d love to know more about your classes and how I can get started. 🇩🇪"
+            )
+          }
+        >
+          <>
+          <span className="chatText">Chat us now</span>
+
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/3670/3670051.png"
+            alt="Chat with BilingualChule"
+          />
+          </>
+        </FlatButton>
+
+
         </>
     )
 }
